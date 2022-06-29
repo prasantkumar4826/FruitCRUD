1b8129e5f19b73262e974b6f3ea62bd5667ea083
prasantkumar4826 <69304510+prasantkumar4826@users.noreply.github.com>
2022-06-29 12:52:05 +0530

    Mat added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,7 +19,11 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
